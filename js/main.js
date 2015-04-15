38f750662966f512d72149be792653d30cf2d567
Gregory Swanson <gregcswanson@gmail.com>
2015-04-16 08:39:15 +1000

    semi working drop down

--- js/main.js
+++ js/main.js
@@ -8,25 +8,98 @@ sampleApp.directive('focusOn', function() {
    };
 });
 
-sampleApp.controller('GridDataController', function($scope){
+sampleApp.directive('selectOnClick', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            element.on('click', function () {
+                this.select();
+            });
+        }
+    };
+});
+
+sampleApp.controller('TimesheetsController', function($scope){
+  $scope.items = [{ }];
+});
+
+sampleApp.controller('TimesheetController', function($scope){
+  $scope.item = { projectId: 2, project: 'two', subprojectId: 0, subproject: ''};
+});
+
+sampleApp.controller('FixedDropDownController', function($scope){
+  // controller for fixed frop downs where the initial value and drop down list is
+  // provided in initialization
+});
+
+sampleApp.controller('SubProjectController', function($scope){
   
 });
 
-sampleApp.controller('GridDataController', function($scope){
+sampleApp.controller('ProjectController', function($scope, $http, $controller){
+  $controller('DropDownController', { $scope: $scope });
+  
+  var parentOnLoad = $scope.onLoad;
+  $scope.onLoad = function(){
+    parentOnLoad();
+    console.log('project onload');
+    $scope.selectItem($scope.item.projectId, $scope.item.project);
+  };
+  
+  $scope.loadList = function(callback){
+    $http.get('api/projects.json').success(function(data){
+      callback(data);
+    });
+  };
+  
+  $scope.selectedValueChange = function(id, text){
+    $scope.item.projectId = id;
+    $scope.item.project = text;
+  };
+  
+  // functions to override
+  // getData(filter);
   
 });
 
-sampleApp.controller('DropDownController', function ($scope) {
-  $scope.items = [
-    {'id': 1, 'name': 'Nexus S', 'active': true },
-    {'id': 2, 'name': 'Motorola XOOM™ with Wi-Fi', 'active': false},
-    {'id': 3, 'name': 'MOTOROLA XOOM™', 'active': false}
-  ];
-  
-  $scope.id = 0;
-  $scope.text = "";
+sampleApp.controller('DropDownController', function ($scope, $http) {
+  $scope.items = [];
+  $scope.dropdownitem = { id: 0, text: ""};
+  //$scope.id = 0;
+  //$scope.text = "";
   $scope.listVisible = false;
   $scope.listHover = false;
+  var scope = $scope;
+  
+  $scope.onLoad = function(){
+    console.log('drop down on load');
+    $scope.loadList();
+  };
+  
+  $scope.loadList = function(callback){
+    var data = [];
+  };
+  
+  $scope.$watchGroup(['listVisible', 'listHover'], function(newValues, oldValues, scope) {
+    if(oldValues[0] === false && oldValues[1] === false) {
+      // load the list
+      $scope.items = [];
+      $scope.loadList(function(data){
+        angular.forEach(data, function(value, key) {
+          this.push({ id : value.id, name: value.title, active: $scope.id == value.id});
+        }, $scope.items);
+      });
+    }
+  });
+  
+  $scope.$watch('item.text', function(newValue, oldValue){
+    console.log(newValue + ', ' + oldValue);
+    $scope.searchForMatch();
+  });
+  
+  $scope.selectedValueChange = function(id, text){
+    
+  };
   
   $scope.onFocus = function(){
     console.log('focus');
@@ -36,6 +109,14 @@ sampleApp.controller('DropDownController', function ($scope) {
   $scope.onBlur = function(){
     console.log('blur');
     $scope.listVisible = false;
+    if (!$scope.listHover) {
+      for (i = 0; i < $scope.items.length; i++) {
+        // make sure the selected item is valid
+        if ($scope.items[i].active === true) {
+          $scope.selectedValueChange();
+        }
+      }
+    }
   };
   
   $scope.onHover = function(){
@@ -58,30 +139,134 @@ sampleApp.controller('DropDownController', function ($scope) {
         $scope.listVisible =true;
       } else {
         // select the next active item
+        $scope.selectNextItem();
       }
     } else if (ev.keyCode == 38){
       // up
+      if($scope.listVisible === false){
+        $scope.listVisible = true;
+      } else {
+        $scope.selectPreviousItem();
+      }
     } else if (ev.keyCode == 37){
       // left
+      if($scope.listVisible === false){
+        $scope.listVisible = true;
+      }
     } else if (ev.keyCode == 39){
       // right
+      if($scope.listVisible === false){
+        $scope.listVisible = true;
+      }
     } else if (ev.keyCode == 27){
       // escape
       if($scope.listVisible === true){
         $scope.listVisible = false;
       }
-    } else if (ev.keyCode == 9){
-      // tab
+    } else if (ev.keyCode == 9 || ev.keyCode == 13){
+      // tab or enter
+      $scope.selectActiveItem();
     } else if (ev.keyCode == 17){
       // ctrl + n for new
-    } 
+    } else {
+      $scope.listVisible = true;
+      $scope.searchForMatch();
+    }
     
   };
   
+  $scope.selectNextItem = function(){
+    var found = false;
+    for (i = 0; i < $scope.items.length; i++)  {
+      if ($scope.items[i].active === true) {
+        found = true;
+        if(i < $scope.items.length - 1) {
+          $scope.items[i].active = false;
+          $scope.items[i + 1].active = true;
+          $scope.item.text = $scope.items[i + 1].name;
+          $scope.item.id = $scope.items[i + 1].id;
+          //$scope.selectedValueChange();
+          break;
+        }
+      }
+    }
+    if (!found && $scope.items.length > 0){
+      $scope.items[0].active = true;
+      $scope.item.text = $scope.items[0].name;
+      $scope.item.id = $scope.items[0].id;
+      //$scope.selectedValueChange();
+    }
+  };
+  
+  $scope.selectPreviousItem = function(){
+    for (i = 0; i < $scope.items.length; i++) {
+      if ($scope.items[i].active === true) {
+        if(i > 0) {
+          $scope.items[i].active = false;
+          $scope.items[i - 1].active = true;
+          $scope.item.text = $scope.items[i - 1].name;
+          $scope.item.id = $scope.items[i - 1].id;
+          //$scope.selectedValueChange();
+        }
+      }
+    }
+  };
+  
   $scope.itemClicked = function(name){
+    for (i = 0; i < $scope.items.length; i++) {
+      if ($scope.items[i].name == name) {
+        $scope.items[i].active = true;
+          $scope.item.text = $scope.items[i].name;
+          $scope.item.id = $scope.items[i].id;
+      }else {
+        $scope.items[i].active = false;
+      }
+    }
     console.log('item clicked');
-    $scope.text = name;
+    $scope.item.text = name;
     $scope.listHover = false;
+    $scope.listVisible = false;
     $scope.$broadcast('newItemAdded');
+  };
+  
+  $scope.selectItem = function(id, text) {
+    $scope.item.id = id;
+    $scope.item.text = text;
+    $scope.selectedValueChanged(id, text);
+  };
+  
+  $scope.searchForMatch = function(){
+    var foundItem = false;
+    console.log(scope.item.text);
+    angular.forEach($scope.items, function(value, key) {
+      if($scope.item.text.length === 0 || foundItem === true) {
+        value.active = false;
+      } else if (value.name.indexOf($scope.item.text) === 0) {
+        value.active = true;
+        foundItem = true;
+      } else {
+        value.active = false;
+      }
+    });
+    if (foundItem === false && $scope.length > 0) {
+      angular.forEach($scope.items, function(value, key) {
+        // contains
+        if(foundItem === false && value.name.indexOf($scope.item.text) > 0) {
+          value.active = true;
+          foundItem = false;
+        }
+      });
+    }
+  };
+  
+  $scope.selectActiveItem = function(){
+    // select the active item in the list or reset the text to blank
+    angular.forEach($scope.items, function(value, key) {
+      if(value.active === true) {
+        $scope.selectItem(value.id, value.text);
+        return;
+      } 
+    });
+    $scope.selectItem(0, '');
   };
 });
